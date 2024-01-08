7cbdc42405bf4d01d7d3c638d9569013e0d60d8b
Rhuan Gonçalves <aquelecarinha_a@hotmail.com>
2024-01-08 03:38:30 -0300

    feat: Progresso no sistema de movimentação.

--- mvp/player/scripts/player.gd
+++ mvp/player/scripts/player.gd
@@ -1,3 +1,19 @@
 extends CharacterBody2D
 
+# Declaração das variáveis de movimento.
+@export var spd: int = 64
+@export var friction: float = 0.2
+@export var acceleration: int = 40
+# Declaração das variáveis de animação.
+@onready var anim = $AnimationPlayer
+@onready var animTree = $AnimationTree
 
+func _ready(): # Função executada automaticamente durante a inicialização da cena.
+	pass
+
+func _physics_process(_delta): # Função executada a cada frame de cena.
+	var dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = dir * spd # Cálculo da velocidade do objeto no ambiente virtual da cena.
+	
+	move_and_slide() # Função nativa que atualiza a posição do objeto com base em sua velocidade.
